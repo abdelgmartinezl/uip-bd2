6d490bdb7c9a1b2939850f52d0167e5a5f142cdc
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2016-05-29 20:17:44 -0500

    Manejo de indices

---
+++ 11.Indices/eliminar.sql
@@ -0,0 +1,34 @@
+/*
+Indices
+-------
+Un índice puede ser creado en una tabla para encontrar data más rápida y eficientemente.
+Los usuarios no pueden ver los índices, ellos solamente sienten las mejoras para hacer
+búsquedas y consultas.
+
+Nota: Realizar una actualización a una tabla indexada toma más tiempo que actualizar una
+tabla sin índices. Por lo tanto, se deben crear índices en columnas y tablas que son
+frecuentemente consultadas.
+
+Sintaxis:
+DROP INDEX nombre_indice ON nombre_tabla;
+*/
+
+/*
+----------------------
+| cliente            |
+----------------------
+| id INT PK          |
+| nombre VARCHAR(50) |
+----------------------
+*/
+
+CREATE TABLE cliente
+(
+id INT PRIMARY KEY,
+nombre VARCHAR(50) NOT NULL
+);
+
+CREATE UNIQUE INDEX nombreIndice
+ON cliente(nombre);
+
+ALTER TABLE cliente DROP INDEX nombreIndice;
